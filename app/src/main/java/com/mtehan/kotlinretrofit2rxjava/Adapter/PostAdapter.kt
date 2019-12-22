b1a2a56fa090d8aeb5869d794cb2e9d63c168f19
metehansoner <40967481+metehansoner@users.noreply.github.com>
2019-12-23 00:28:42 +0300

    Adapter OnClick

--- app/src/main/java/com/mtehan/kotlinretrofit2rxjava/Adapter/PostAdapter.kt
+++ app/src/main/java/com/mtehan/kotlinretrofit2rxjava/Adapter/PostAdapter.kt
@@ -3,6 +3,7 @@ package com.mtehan.kotlinretrofit2rxjava.Adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.mtehan.kotlinretrofit2rxjava.Model.Post
 import com.mtehan.kotlinretrofit2rxjava.R
@@ -24,7 +25,10 @@ class PostAdapter(internal var context: Context, internal var postList: List<Pos
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         holder.txt_author.text = postList[position].userId.toString()
         holder.txt_title.text = postList[position].title
-        holder.txt_content.text =
-            StringBuilder(postList[position].body.substring(0, 20)).append("....").toString()
+        holder.txt_content.text = StringBuilder(postList[position].body.substring(0, 20)).append("....").toString()
+
+        holder.itemView.setOnClickListener {
+        Toast.makeText(holder.itemView.context,"${ postList[position].title }",Toast.LENGTH_LONG).show()
+        }
     }
 }
